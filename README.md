39b31f81b5e10db7762ca69fec8503c882760032
Joseph Fox-Rabinovitz <joseph.r.fox-rabinovitz@nasa.gov>
2018-08-23 16:23:47 -0400

    DOC: Added basic documentation pages

---
+++ README.md
@@ -0,0 +1,8 @@
+scikit-guess
+============
+
+This scikit contains methods for computing fast, non-iterative estimates of
+fitting parameters for common functions. The estimates may be used as-is on
+their own, or refined through non-linear optimization algorithms. The name of
+the scikit comes from the fact that estimates are a good initial guess for the
+optimal fitting parameters.
--- doc/conf.py
+++ doc/conf.py
@@ -47,6 +47,7 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -82,7 +83,12 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    html_theme = 'alabaster'
+else:
+    html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
