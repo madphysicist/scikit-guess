0744553776aacb219a495023dd8b4f674a6ccb51
Joseph Fox-Rabinovitz <madphysicist@users.noreply.github.com>
2021-02-25 19:36:26 -0600

    ENH/BUG: Optimized/fixed phase calculation
    - Corrected dependence of phase on sign of amplitude
    - Added computation of phase based on median wavelength segment
    - Documented local variables better
    - Improved debug plotting

--- src/skg/sin.py
+++ src/skg/sin.py
@@ -46,7 +46,8 @@ An alternative to this method is Jean Jacquelin's method.
 """
 
 from numpy import (
-    concatenate, full_like, searchsorted, median, std, sqrt, sin, pi
+    argpartition, concatenate, full_like, searchsorted, median, std, sqrt,
+    sin, pi
 )
 
 from .cluster1d import cluster1d
@@ -95,9 +96,15 @@ def sin_fit(t, y, sorted=True, _debug=False):
       are entirely the work of the author. A peer reviewed paper is currently
       in the works.
     """
+    # t: Time of raw readings
+    # y: Sinusoidal readings
     t, y = preprocess_pair(t, y, sorted)
+    # z: The interpolated crossings (in units of `t`)
+    # bias: mean value of `y`
     z, bias = roots(t, y, return_bias=True)
+    # breaks: indices into `z`, starting with zero
     breaks = cluster1d(z, sensitivity=2)
+    # xings: medians of each group of `z` delimited by `breaks`, units of `t`
     xings = medians(z, breaks)
 
     if _debug:
@@ -105,28 +112,36 @@ def sin_fit(t, y, sorted=True, _debug=False):
 
         plt.figure()
         p1 = plt.subplot(3, 1, 1)
-        plt.vlines(z, 0, 1)
-        plt.vlines(0.5 * (z[breaks[1:]] + z[breaks[:-1] + 1]), 0, 1, 'r', ':')
-        plt.scatter(xings, full_like(xings, 0.5), c='m', zorder=10)
-        plt.title('Initial grouping')
+        p1.vlines(z, 0, 1)
+        p1.vlines(0.5 * (z[breaks[1:]] + z[breaks[:-1] + 1]), 0, 1,
+                   color='r', linestyle=':')
+        p1.scatter(xings, full_like(xings, 0.5), c='m', zorder=10)
+        p1.set_title('Initial grouping')
 
     # Post process crossings
     # Speed tested
+    # indices: locations where `xings` fall into the original `t`
     indices = concatenate(([0], searchsorted(t, xings, side='right')))
 
+    # sign: True for each region between a pair of `indices` that has at least
+    #   half its points above the bias, False for the other regions. Same size
+    #   as `indices` because of implicit last element.
     sign = (sums(y > bias, indices) > 0.5 * counts(indices, t.size))
+    # mask: selection of regions that have opposite signs. Crossings that are
+    #   between regions with the same sign are automatically deemed fake.
     mask = (sign[:-1] != sign[1:])
 
     if _debug:
         from matplotlib import pyplot as plt
 
-        plt.subplot(3, 1, 2, sharex=p1)
+        p2 = plt.subplot(3, 1, 2, sharex=p1)
         for sig, start, end in zip(sign, indices, ends(indices, t.size)):
-            plt.plot(t[start:end], y[start:end], 'b' if sig else 'r')
-        plt.plot(xings, full_like(xings, bias), 'mx', zorder=10)
-        plt.plot(xings[~mask], full_like(xings[~mask], bias), 'mo', zorder=10)
-        plt.title('Checking for sign flip')
+            p2.plot(t[start:end], y[start:end], 'b' if sig else 'r')
+        p2.plot(xings, full_like(xings, bias), 'mx', zorder=10)
+        p2.plot(xings[~mask], full_like(xings[~mask], bias), 'mo', zorder=10)
+        p2.set_title('Checking for sign flip')
 
+    # Remove invalid `xings`
     xings = xings[mask]
 
     # Extract wavelengths
@@ -135,15 +150,28 @@ def sin_fit(t, y, sorted=True, _debug=False):
     wavelengths = wl_end - wl_start
     wavelength = median(wavelengths)
 
+    # Location of median wavelength    
+    k = (wavelengths.size - 1) // 2
+    wavelength_index = argpartition(wavelengths, k)[k]
+    offset = (wavelength_index % 2 == 1) ^ sign[0]
+
     if _debug:
-        plt.subplot(3, 1, 3, sharex=p1)
+        p3 = plt.subplot(3, 1, 3, sharex=p1)
         for i, (start, end) in enumerate(zip(wl_start, wl_end)):
-            plt.plot([start, end], [i % 3, i % 3], '-ro')
-        plt.title('Alternating wavelengths')
+            color = 'g' if i == wavelength_index else 'r'
+            p3.plot([start, end], [i % 3, i % 3], '-o', c=color)
+        p3.set_title('Alternating wavelengths')
 
     omega = 2.0 * pi / wavelength
     amplitude = std(y) * sqrt(2.0) # Sine has crest factor ~= sqrt(2)
-    phase = omega * (xings[sign[0].astype(int)] % (2.0 * pi))
+    full_phase = xings[wavelength_index + offset]
+    phase = omega * (full_phase % wavelength)
+
+    if _debug:
+        lims = p2.get_xlim()
+        p2.arrow(0, bias + 0.1 * amplitude, full_phase, 0, color='orange')
+        p2.plot([full_phase] * 2, [bias + 0.12 * amplitude, 0], color='orange')
+        p2.set_xlim(lims)
 
     return amplitude, omega, phase, bias
 
@@ -179,4 +207,3 @@ def model(t, a, omega, phi, b):
 
 
 sin_fit.model = model
-
